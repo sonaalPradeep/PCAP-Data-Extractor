72560fad2038ee2ff47c1c0a140481c5db85f04c
root <root@localhost.localdomain>
2020-05-24 09:32:28 +0530

    can extract txt files

--- extr_ftp.py
+++ extr_ftp.py
@@ -15,7 +15,6 @@ def port_condition(packet):
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser("Extract data from PCAP files. Defaults to extracting from FTP packets")
-	# group = parser.add_mutually_exclusive_group()
 	parser.add_argument("file", help = "PCAP file to load")
 	parser.add_argument("-g", "--gui", action = 'store_true', help = "open in gui")
 	parser.add_argument("-s", "--save", action = 'store_true', help = "save raw data in file")
@@ -32,23 +31,33 @@ if __name__ == '__main__':
 
 	else:
 		packet_list = rdpcap(args.file)	
-		raw_data = []
-		req_data = []
+		file_name, file_format = "", ""
 
 		if args.save and ('raw.txt' in os.listdir()):
 			os.remove('raw.txt')
 
 		for ind in range(len(packet_list)):
 			try:
-				# packet_list[ind].show()
-				raw_data.append(str(packet_list[ind]['Raw'].load).lstrip('b'))
+				line = str(packet_list[ind]['Raw'].load).lstrip("'b").rstrip("'")
 				if args.save:
 					with open('raw.txt', 'a+') as f:
-						f.write(raw_data[-1] + '\n')
+						f.write(line + '\n')
+				
+				if line[:4] == 'RETR':
+					file_name, file_format = line.split()[-1].split('.')
+					file_format = file_format[:-4]
 
-				if(port_condition(packet_list[ind])):
-					# packet_list[ind].show()
-					req_data.append(packet_list[ind])
+					if file_name + '.' + file_format in os.listdir():
+						os.remove(file_name + '.' + file_format)
+					continue
+				elif line == r'226 Transfer complete.\r\n':
+					file_name, file_format = "", ""
+					continue
+
+				if file_name != "" and port_condition(packet_list[ind]):
+					with open(file_name + '.' + file_format, 'a+') as f:
+						f.write('\n'.join(line.split(r'\n')))
+				
 			except:
 				continue
 
